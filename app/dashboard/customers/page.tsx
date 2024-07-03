3ad88a556ed6d5e9896e9447e87a997a576dc90c
Leighton Grant <oneal.grant@outlook.com>
2024-07-03 11:08:30 +0100

    adds search and pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import { fetchCustomers } from "@/app/lib/data";
+import { error } from "console";
+
+const response = await fetchCustomers()
+
 export default function Customers() {
-  return <p>Customers Page</p>;
+  console.log(response)
+  
+  return <>
+    <h1 className="text-4xl">Customers Page</h1>
+    <ul>{response.map(customer => <li key={customer.id}>{customer.name }</li>)}</ul>
+  </>
 }
